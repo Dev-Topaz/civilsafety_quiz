7078e3cb8564cabda5241ab034c0c1255adb6596
Sophie <bolesalavb@gmail.com>
2021-09-29 20:35:54 +0900

    change api root url

--- lib/const.dart
+++ lib/const.dart
@@ -1,3 +1,3 @@
-const String API_ROOT_URL = 'http://civilsafety.fluee123.host/public/api/';
-// const String API_ROOT_URL = 'https://civilsafetyonline.com.au/quizmaker/public/api/';
+// const String API_ROOT_URL = 'http://civilsafety.fluee123.host/public/api/';
+const String API_ROOT_URL = 'https://civilsafetyonline.com.au/quizmaker/public/api/';
 const debug = true;
